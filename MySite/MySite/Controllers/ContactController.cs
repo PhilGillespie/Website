fb0106eb590134299cf81710d63164bcc5f51458
PhilGillespie <philgillespie@hotmail.com>
2016-08-02 20:41:50 +1200

    initial commit

--- MySite/MySite/Controllers/ContactController.cs
+++ MySite/MySite/Controllers/ContactController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 using MySite.Models;
 using System.Net.Mail;
+using System.Configuration;
 
 namespace MySite.Controllers
 {
@@ -23,20 +24,21 @@ namespace MySite.Controllers
 
         public ActionResult FormPost(ContactModel post)
         {
+            var appSettings = ConfigurationManager.AppSettings;
             ContactModel model = new ContactModel()
             {
                 Name = post.Name,
                 Email = post.Email,
                 Message = post.Message
             };
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            SmtpClient client = new SmtpClient(appSettings["SMTPClient"], Int32.Parse(appSettings["SMTPClientPort"]));
             MailAddress from = new MailAddress("test@test.com");
-            MailAddress to = new MailAddress("philgillespie@hotmail.com");
+            MailAddress to = new MailAddress(appSettings["EmailToAddress"]);
             MailMessage msg = new MailMessage(from, to);
             msg.Body = post.Name + " : " + post.Email + " : " + post.Message;
             msg.Subject = "Message from website";
             client.Host = "smtp.gmail.com";
-            client.Credentials = new System.Net.NetworkCredential("phil.s.gillespie@gmail.com", "me$$ugah77");
+            client.Credentials = new System.Net.NetworkCredential(appSettings["SiteEmailAddress"], appSettings["SiteEmailPassword"]);
             client.EnableSsl = true;
             client.Send(msg);
             return RedirectToCurrentUmbracoPage();
