337c3911db6f8598bae41592e5e144056c80cd6f
PhilGillespie <philgillespie@hotmail.com>
2016-08-02 13:33:57 +1200

    initial commit

--- MySite/MySite/Controllers/ContactController.cs
+++ MySite/MySite/Controllers/ContactController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 using MySite.Models;
+using System.Net.Mail;
 
 namespace MySite.Controllers
 {
@@ -17,6 +18,22 @@ namespace MySite.Controllers
 
         public ActionResult FormPost(ContactModel post)
         {
+            ContactModel model = new ContactModel()
+            {
+                Name = post.Name,
+                Email = post.Email,
+                Message = post.Message
+            };
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            MailAddress from = new MailAddress("test@test.com");
+            MailAddress to = new MailAddress("philgillespie@hotmail.com");
+            MailMessage msg = new MailMessage(from, to);
+            msg.Body = post.Name + " : " + post.Email + " : " + post.Message;
+            msg.Subject = "Message from website";
+            client.Host = "smtp.gmail.com";
+            client.Credentials = new System.Net.NetworkCredential("phil.s.gillespie@gmail.com", "me$$ugah77");
+            client.EnableSsl = true;
+            client.Send(msg);
             return RedirectToCurrentUmbracoPage();
         }
     }
